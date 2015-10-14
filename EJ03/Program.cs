ae87a015c4a8faab574e04277f9b179e9b7f0d9f
RamaRivera <ramarivera@gmail.com>
2015-10-14 18:33:26 -0300

    Prueba Concurrencia Ramiro

--- EJ03/Program.cs
+++ EJ03/Program.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EJ03
 {
     class Program
     {
+        static void ImprimirLineas()
+        {
+
+        }
+
         static void Main(string[] args)
         {
             //holisssss
