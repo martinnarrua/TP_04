4426225bbd5d859ceeb1cb95b05af73c180256ef
RamaRivera <ramarivera@gmail.com>
2015-10-21 18:39:31 -0300

    Comentario de las Excepciones

--- EJ04/DesbordamientoException.cs
+++ EJ04/DesbordamientoException.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EJ02
+namespace EJ04
 {
-    class DesbordamientoException: OverflowException
+    /// <summary>
+    /// Representa una excepcion que se arroja cuando el saldo actual de una <see cref="Cuenta"/> mas el monto que se desea acreditar genera un desbordamiento.
+    /// </summary>
+    class DesbordamientoException : OverflowException
     {
-        public DesbordamientoException():base() { }
+        /// <summary>
+        /// Inicializa una nueva instancia de <see cref="DesbordamientoException"/>. Constructor por defecto
+        /// </summary>
+        public DesbordamientoException() : base() { }
 
-
+        /// <summary>
+        /// Inicializa una nueva instancia de <see cref="DesbordamientoException"/> con un mensaje de error especifico.
+        /// </summary>
+        /// <param name="pMensaje">Mensaje que explica la causa de la excepcion</param>
         public DesbordamientoException(string pMensaje) : base(pMensaje) { }
+
     }
 }
