9762a7bb41d6841a74f0a1f8bf6ca8d79c602d56
RamaRivera <ramarivera@gmail.com>
2015-10-21 20:12:24 -0300

    Commit Inicial

---
+++ EJ05.Test/UserCodeAscendingComparerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EJ05.Test
+{
+    [TestClass]
+    public class UserCodeAscendingComparerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
