f5e55795f9627f5a9742015dafd49aff61bf9833
RamaRivera <ramarivera@gmail.com>
2015-10-23 19:03:42 -0300

    Commit inicial

---
+++ EJ05/UsuarioNoEncontradoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ05
+{
+    public class UsuarioNoEncontradoException : KeyNotFoundException
+    {
+        public UsuarioNoEncontradoException() : base() { }
+    }
+}
