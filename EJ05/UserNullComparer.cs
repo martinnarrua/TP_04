b213128e430787abeeb35aacdff1d4c3b7a61b02
RamaRivera <ramarivera@gmail.com>
2015-10-23 19:34:28 -0300

    commit inicial

---
+++ EJ05/UserNullComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace EJ05
+{
+    /// <summary>
+    /// Comparador Nulo de <see cref="Usuario"/>. Al utilizarse no afecta el ordenamiento actual.
+    /// Implementacion del patron Null Object
+    /// </summary>
+    internal class UserNullComparer : IComparer<Usuario>
+    {
+        /// <summary>
+        /// Compara dos <see cref="Usuario"/>
+        /// </summary>
+        /// <param name="pUsuario1">Primer <see cref="Usuario"/></param>
+        /// <param name="pUsuario2">Segundo <see cref="Usuario"/></param>
+        /// <returns> 0, independientemente de los usuarios pasados como parametro</returns>
+        public int Compare(Usuario pUsuario1, Usuario pUsuario2)
+        {
+            return 0;      
+        }
+
+    }
+}
