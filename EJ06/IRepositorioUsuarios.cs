18b091cadf56fb7ac642697c8d348d8c13b7e322
RamaRivera <ramarivera@gmail.com>
2015-10-28 20:00:05 -0300

    Commit inicial

---
+++ EJ06/IRepositorioUsuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ06
+{
+    public interface IRepositorioUsuarios
+    {
+        void Agregar(Usuario pUsuario);
+
+        void Actualizar(Usuario pUsuario);
+
+        void Eliminar(string pCodigo);
+
+        IList<Usuario> ObtenerTodos();
+
+        Usuario ObtenerPorCodigo(string pCodigo);
+
+        IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparador);
+    }
+}
---
+++ EJ06/RepositorioUsuarios.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ06
+{
+    public class RepositorioUsuarios: IRepositorioUsuarios
+    {
+        private SortedDictionary<string, Usuario> iUsuarios;
+
+        public RepositorioUsuarios()
+        {
+            this.Usuarios = new SortedDictionary<string, Usuario>();
+        }
+
+        private SortedDictionary<string, Usuario> Usuarios
+        {
+            get { return this.iUsuarios; }
+            set { this.iUsuarios = value; }
+        }
+        private IRepositorioUsuarios AsIRepositorioUsuarios
+        {
+            get { return this; }
+        }
+
+        void IRepositorioUsuarios.Agregar(Usuario pUsuario)
+        {
+            Usuarios.Add(pUsuario.Codigo, pUsuario);
+        }
+
+        void IRepositorioUsuarios.Actualizar(Usuario pUsuario)
+        {
+            if (Usuarios.ContainsKey(pUsuario.Codigo))
+            {
+                Usuarios[pUsuario.Codigo] = pUsuario;
+            }
+            else
+            {
+                UsuarioNoEncontradoException excepcion = new UsuarioNoEncontradoException(String.Format("Usuario con el codigo {0} no encontrado", pUsuario.Codigo));
+            }
+        }
+
+        void IRepositorioUsuarios.Eliminar(string pCodigo)
+        {
+            if (Usuarios.ContainsKey(pCodigo))
+            {
+                Usuarios.Remove(pCodigo);
+            }
+            else
+            {
+                UsuarioNoEncontradoException excepcion = new UsuarioNoEncontradoException(String.Format("Usuario con el codigo {0} no encontrado", pCodigo));
+            }
+        }
+        IList<Usuario> IRepositorioUsuarios.ObtenerTodos()
+        {
+            List<Usuario> lLista = (List<Usuario>) this.ObtenerSinOrdenar();
+            lLista.Sort();
+            return lLista;
+        }
+        
+        Usuario IRepositorioUsuarios.ObtenerPorCodigo(string pCodigo)
+        {
+            List<Usuario> lLista = (List<Usuario>) this.AsIRepositorioUsuarios.ObtenerTodos();
+            Usuario lUsuario = null;
+            if (Usuarios.ContainsKey(pCodigo))
+            {
+                lUsuario = Usuarios[pCodigo];
+            }
+            else
+            {
+                UsuarioNoEncontradoException excepcion = new UsuarioNoEncontradoException(String.Format("Usuario con el codigo {0} no encontrado", pCodigo));
+                //revisar esto
+                //TODO: DOBLEMENTE REVISAR ESTO
+            }
+            return lUsuario;
+        }
+
+        IList<Usuario> IRepositorioUsuarios.ObtenerOrdenadosPor(IComparer<Usuario> pComparador)
+        {
+            List<Usuario> lLista = (List<Usuario>) this.ObtenerSinOrdenar();
+            lLista.Sort(pComparador);
+            return lLista;
+        }
+
+
+        private IList<Usuario> ObtenerSinOrdenar()
+        {
+            List<Usuario> lLista = this.Usuarios.Values.ToList();
+            return lLista;
+        }
+    }
+}
