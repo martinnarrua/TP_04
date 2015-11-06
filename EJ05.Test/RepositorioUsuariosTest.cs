4039a174bafda6b53960c5ed8eb5eb49a60589d3
RamaRivera <ramarivera@gmail.com>
2015-11-06 18:33:03 -0300

    Testing Terminado

--- EJ05.Test/RepositorioUsuariosTest.cs
+++ EJ05.Test/RepositorioUsuariosTest.cs
@@ -11,7 +11,7 @@ namespace EJ05.Test
     public class RepositorioUsuariosTest
     {
         [TestMethod]
-        public void ObtenerOrdenadosPor_WithDefaultSort()
+        public void ObtenerOrdenadosPor_WithCaminoFeliz()
         {
             Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
             Usuario lUsuario2 = new Usuario { Codigo = "MMMM", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
@@ -24,15 +24,15 @@ namespace EJ05.Test
             lRepositorio.Agregar(lUsuario1);
             lRepositorio.Agregar(lUsuario2);
 
-            lListaResultado = (List<Usuario>) lRepositorio.ObtenerTodos();
-           
+            lListaResultado = (List<Usuario>)lRepositorio.ObtenerTodos();
+
             CollectionAssert.AreEqual(lLista, lListaResultado);
         }
 
         [TestMethod]
         public void ObtenerOrdenadosPor_WithCodAscSort()
         {
-            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa" , CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
             Usuario lUsuario2 = new Usuario { Codigo = "MMMM", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
             Usuario lUsuario3 = new Usuario { Codigo = "ZZZZ", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
             IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
@@ -44,7 +44,7 @@ namespace EJ05.Test
             lRepositorio.Agregar(lUsuario1);
             lRepositorio.Agregar(lUsuario2);
 
-            lListaResultado = (List<Usuario>) lRepositorio.ObtenerOrdenadosPor(lComparador);
+            lListaResultado = (List<Usuario>)lRepositorio.ObtenerOrdenadosPor(lComparador);
 
             CollectionAssert.AreEqual(lLista, lListaResultado);
         }
@@ -229,9 +229,276 @@ namespace EJ05.Test
             catch (UsuarioExistenteException) { }
         }
 
+        [TestMethod]
+        public void Agregar_WithCaminoFeliz_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "ZZZZ", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario3);
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Agregar(lUsuario2);
+            }
+            catch (System.Exception) { }
+
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        public void Actualizar_WithUsuarioNulo_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = null;
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario3);
+                lRepositorio.Actualizar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario2);
+                Assert.Fail();
+
+            }
+            catch (ArgumentNullException) { }
+        }
+
+        [TestMethod]
+        public void Actualizar_WithCodigoNulo_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = null, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario3);
+                lRepositorio.Actualizar(lUsuario2);
+                Assert.Fail();
+
+            }
+            catch (ArgumentNullException) { }
+        }
+
+        [TestMethod]
+        public void Actualizar_WithCodigoVacio_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = String.Empty, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario3);
+                lRepositorio.Actualizar(lUsuario2);
+                Assert.Fail();
+
+            }
+            catch (ArgumentException) { }
+        }
+
+        [TestMethod]
+        public void Actualizar_WithUsuarioNoEncontrado_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "ZZZZ", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario3);
+                lRepositorio.Actualizar(lUsuario2);
+                Assert.Fail();
+
+            }
+            catch (UsuarioNoEncontradoException) { }
+        }
+
+        [TestMethod]
+        public void Actualizar_WithCaminoFeliz_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario1);
+                lRepositorio.Actualizar(lUsuario3);
+            }
+            catch (System.Exception) { }
+
+            Assert.IsTrue(true);
+        }
 
 
 
+
+        [TestMethod]
+        public void Eliminar_WithCodigoNulo_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = null, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Eliminar(lUsuario2.Codigo);
+                Assert.Fail();
+
+            }
+            catch (ArgumentNullException) { }
+        }
+
+        [TestMethod]
+        public void Eliminar_WithCodigoVacio_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = String.Empty, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Eliminar(lUsuario2.Codigo);
+                Assert.Fail();
+
+            }
+            catch (ArgumentException) { }
+        }
+
+        [TestMethod]
+        public void Eliminar_WithUsuarioNoEncontrado_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "ZZZZ", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Eliminar(lUsuario3.Codigo);
+                Assert.Fail();
+
+            }
+            catch (UsuarioNoEncontradoException) { }
+        }
+
+        [TestMethod]
+        public void Eliminar_WithCaminoFeliz_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.Eliminar(lUsuario1.Codigo);
+                lRepositorio.Agregar(lUsuario2);
+                lRepositorio.Eliminar(lUsuario2.Codigo);
+                lRepositorio.Agregar(lUsuario3);
+                lRepositorio.Eliminar(lUsuario3.Codigo);
+            }
+            catch (System.Exception) { }
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ObtenerPorCodigo_WithCodigoNulo_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = null, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario();
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.ObtenerPorCodigo(lUsuario2.Codigo);
+                Assert.Fail();
+
+            }
+            catch (ArgumentNullException) { }
+        }
+
+        [TestMethod]
+        public void ObtenerPorCodigo_WithCodigoVacio_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = String.Empty, NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.ObtenerPorCodigo(lUsuario2.Codigo);
+                Assert.Fail();
+
+            }
+            catch (ArgumentException) { }
+        }
+
+        [TestMethod]
+        public void ObtenerPorCodigo_WithUsuarioNoEncontrado_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "AAAA", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "ZZZZ", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            try
+            {
+                lRepositorio.Agregar(lUsuario1);
+                lRepositorio.ObtenerPorCodigo(lUsuario2.Codigo);
+                Assert.Fail();
+
+            }
+            catch (UsuarioNoEncontradoException) { }
+        }
+
+        [TestMethod]
+        public void ObtenerPorCodigo_WithCaminoFeliz_Fails()
+        {
+            Usuario lUsuario1 = new Usuario { Codigo = "MMMM", NombreCompleto = "Martin Arrúa", CorreoElectronico = "Martin94.profugo@hotmail.com" };
+            Usuario lUsuario2 = new Usuario { Codigo = "RRRR", NombreCompleto = "Ramiro Rivera", CorreoElectronico = "Ramarivera@gmail.com" };
+            Usuario lUsuario3 = new Usuario { Codigo = "AAAA", NombreCompleto = "Agustina Mannise", CorreoElectronico = "Agusmn95@gmail.com" };
+            Usuario lUsuario4 = new Usuario();
+            IRepositorioUsuarios lRepositorio = new RepositorioUsuarios();
+
+            lRepositorio.Agregar(lUsuario1);
+            lRepositorio.Agregar(lUsuario2);
+            lRepositorio.Agregar(lUsuario3);
+            lUsuario4 = lRepositorio.ObtenerPorCodigo(lUsuario2.Codigo);
+
+
+
+            Assert.AreEqual(lUsuario2, lUsuario4);
+        }
 
     }
 }
